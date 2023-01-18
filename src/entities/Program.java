d0cf4f210b8f668813933bf0e7a5f2a36eed533b
Valdinei Tinoco <valdinei_ct@hotmail.com>
2023-01-17 23:13:05 -0300

    Eclise Vetor

---
+++ src/entities/Program.java
@@ -0,0 +1,39 @@
+package entities;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc= new Scanner (System.in);
+		
+		int horaInicial = sc.nextInt();
+		int horaFinal = sc.nextInt();
+		int duracao;
+		
+		if(horaInicial < horaFinal) {
+			duracao = horaFinal - horaInicial;
+		}
+		
+		else {
+			duracao = 24-horaInicial + horaFinal;
+		}
+		
+		System.out.println("O jogo durou"  + duracao +  "hora(s)");
+		
+		
+		
+		sc.close();
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
